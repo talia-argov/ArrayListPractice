0e232a6dbda823db360f82f8c01f2a67b2492319
talia-argov <talia_argov24@milton.edu>
2023-04-03 09:51:51 -0400

    Initial commit

---
+++ src/Numbers.java
@@ -0,0 +1,12 @@
+public class Numbers {
+
+    public int count;
+    public boolean even;
+    public boolean odd;
+
+    public Numbers(int pCount){
+        count = pCount;
+    }
+    public void printInfo(){System.out.println(count); }
+
+}
---
+++ src/Practice.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Practice {
+
+    public Number list2;
+    public Number list1;
+    ArrayList<Numbers>;
+
+    public static void main(String[] args) {
+        Practice p = new Practice();
+
+    } //if you're seeing this ms k sorry its very messy, I'm still trying to figure it out
+
+    public Practice(){
+        list1 = new ArrayList<>();
+        list1.add(Numbers(3));
+// construct an arraylist called numbers that contains all integers from 0 to 15.
+        for(int x = 0; x < list1.length; x = x + 1){
+            list1[x] = new Numbers();
+            [].printInfo();
+        }
+    }
+
+}
